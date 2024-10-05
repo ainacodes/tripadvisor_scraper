fb09b22ae80f9e5788cdabe6b61115f53a2bdce4
ainacodes <noraina.nordin16@gmail.com>
2024-10-06 00:46:01 +0800

    update user-agent

--- restaurant_review_details.py
+++ restaurant_review_details.py
@@ -1,15 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 import logging
-from fake_useragent import UserAgent
 import csv
 import time
 
 
 def setup_request():
-    user_agent = UserAgent()
     headers = {
-        "User-Agent": user_agent.random,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br",
--- restaurant_review_details_pagination.py
+++ restaurant_review_details_pagination.py
@@ -3,13 +3,11 @@ from bs4 import BeautifulSoup
 import logging
 import csv
 import time
-from fake_useragent import UserAgent
 
 
 def setup_request():
-    user_agent = UserAgent()
     headers = {
-        "User-Agent": user_agent.random,
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
         "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
         "Accept-Language": "en-US,en;q=0.5",
         "Accept-Encoding": "gzip, deflate, br",
